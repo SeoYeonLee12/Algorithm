0fac62d0c8f844a6aafc9b76897d95a099e5339a
SeoYeonLee12 <134525851+SeoYeonLee12@users.noreply.github.com>
2025-08-13 02:10:31 +0900

    boj 2292: 시간: 108ms

---
+++ src/boj/N2292.java
@@ -0,0 +1,23 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class N2292 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+
+        int N= Integer.parseInt(br.readLine());
+
+        int count=1;
+        int range=1;
+
+        while(N>range){
+            range += 6*count;
+            count++;
+        }
+
+        System.out.println(count);
+    }
+}
